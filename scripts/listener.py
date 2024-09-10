233740838a8ba5875e88bd322e2cdc00493f6abe
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-10 14:07:23 +1000

    Add landing message

--- scripts/listener.py
+++ scripts/listener.py
@@ -1,16 +1,23 @@
 import rospy
 import espeakng
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 
 def test_callback(data):
     rospy.loginfo('received test msg: \"%s\"', data.data)
     espeakng.Speaker().say(data.data)
+
+def landing_callback(data):
+    if data.data: # bool is True
+        rospy.loginfo('received landing msg: \"%s\"', data.data)
+        espeakng.Speaker().say('Landing complete')
 
 def listener():
     rospy.init_node('gcs_vocal', anonymous=True)
     rospy.loginfo('starting gcs vocalizer listener')
 
     rospy.Subscriber('vocal/test', String, test_callback)
+    rospy.Subscriber('vocal/land', Bool, test_callback)
 
     rospy.spin()
 
