37dc8a54a6c4f883894d46be2a16abea716168f2
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-10 14:17:20 +1000

    Add Aruch marker detection

--- scripts/listener.py
+++ scripts/listener.py
@@ -2,6 +2,7 @@ import rospy
 import espeakng
 from std_msgs.msg import String
 from std_msgs.msg import Bool
+from std_msgs.msg import Int32
 
 def test_callback(data):
     rospy.loginfo('received test msg: \"%s\"', data.data)
@@ -12,12 +13,17 @@ def landing_callback(data):
         rospy.loginfo('received landing msg: \"%s\"', data.data)
         espeakng.Speaker().say('Landing complete')
 
+def aruco_callback(data):
+    rospy.loginfo('received aruco id: \"%s\"', data.data)
+    espeakng.Speaker().say("Detected aruco marker with ID " + str(data.data))
+
 def listener():
     rospy.init_node('gcs_vocal', anonymous=True)
     rospy.loginfo('starting gcs vocalizer listener')
 
     rospy.Subscriber('vocal/test', String, test_callback)
     rospy.Subscriber('vocal/land', Bool, test_callback)
+    rospy.Subscriber('vocal/aruco', Int32, aruco_callback)
 
     rospy.spin()
 
