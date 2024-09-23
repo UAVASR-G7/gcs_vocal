3c058fc17a45c6c12ce8c85902188591d1f23dc2
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-24 02:01:13 +1000

    Modified msg callback var and format

--- scripts/listener.py
+++ scripts/listener.py
@@ -4,18 +4,18 @@ from std_msgs.msg import String
 from std_msgs.msg import Bool
 from std_msgs.msg import Int32
 
-def test_callback(data):
-    rospy.loginfo('received test msg: \"%s\"', data.data)
-    espeakng.Speaker().say(data.data)
+def test_callback(msg):
+    rospy.loginfo("received test msg: '%s'" % msg.data)
+    espeakng.Speaker().say(msg.data)
 
-def landing_callback(data):
-    if data.data: # bool is True
-        rospy.loginfo('received landing msg: \"%s\"', data.data)
+def landing_callback(msg):
+    if msg.data: # bool is True
+        rospy.loginfo("received landing msg")
         espeakng.Speaker().say('Landing complete')
 
-def aruco_callback(data):
-    rospy.loginfo('received aruco id: \"%s\"', data.data)
-    espeakng.Speaker().say("Detected aruco marker with ID " + str(data.data))
+def aruco_callback(msg):
+    rospy.loginfo("received aruco id: '%d'" % msg.data)
+    espeakng.Speaker().say("Detected aruco marker with ID %d" % msg.data)
 
 def listener():
     rospy.init_node('gcs_vocal', anonymous=True)
