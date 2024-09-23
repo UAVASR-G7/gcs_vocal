fae74f66d24013c87c15f99ffb375ab0506354cc
Jayden Grubb <jaydengrubb1@gmail.com>
2024-09-24 03:03:32 +1000

    Add target detection callback

--- scripts/listener.py
+++ scripts/listener.py
@@ -3,6 +3,7 @@ import espeakng
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 from std_msgs.msg import Int32
+from spar_msgs.msg import TargetLocalisation
 
 def test_callback(msg):
     rospy.loginfo("received test msg: '%s'" % msg.data)
@@ -17,6 +18,10 @@ def aruco_callback(msg):
     rospy.loginfo("received aruco id: '%d'" % msg.data)
     espeakng.Speaker().say("Detected aruco marker with ID %d" % msg.data)
 
+def target_callback(msg):
+    rospy.loginfo("received target of type: '%s'" % msg.data.target_label)
+    espeakng.Speaker().say("%s found at %.1f in X and %.1f in Y" % (msg.data.target_label, msg.data.frame_x, msg.data.frame_y))
+
 def listener():
     rospy.init_node('gcs_vocal', anonymous=True)
     rospy.loginfo('starting gcs vocalizer listener')
@@ -24,6 +29,7 @@ def listener():
     rospy.Subscriber('vocal/test', String, test_callback)
     rospy.Subscriber('vocal/land', Bool, landing_callback)
     rospy.Subscriber('vocal/aruco', Int32, aruco_callback)
+    rospy.Subscriber('vocal/target', TargetLocalisation, target_callback)
 
     rospy.spin()
 
